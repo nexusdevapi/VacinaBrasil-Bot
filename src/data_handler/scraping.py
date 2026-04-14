b7e74e318c3dbdbd168656db12542185a4b9d4c5
Miguel Gomes <miguelcisco1@proton.me>
2026-04-14 18:18:07 -0300

    chore: mover extrair_idade para utils/helpers

--- src/data_handler/scraping.py
+++ src/data_handler/scraping.py
@@ -1,28 +1,10 @@
 import json
-import re
 from io import BytesIO
 
 import requests
 import pdfplumber
 
-# obtendo números da faixa etária
-def extrair_idade(idade_texto):
-    if not idade_texto:
-        return None, None
-
-    idade_texto = str(idade_texto).lower()
-
-    if 'ao nascer' in idade_texto:
-        return 0, 0
-
-    numeros = re.findall(r'\d+', idade_texto)
-
-    if len(numeros) >= 2:
-        return int(numeros[0]), int(numeros[1])
-    elif len(numeros) == 1:
-        return int(numeros[0]), None
-    else:
-        return None, None
+from utils.helpers import extrair_idade
 
 # calendários relevantes disponíveis em https://www.gov.br/saude/pt-br/vacinacao/arquivos/
 urls = [

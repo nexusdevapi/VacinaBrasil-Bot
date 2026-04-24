05109594d14af0bb3b418129993bfae308e738f9
Miguel Gomes <miguelcisco1@proton.me>
2026-04-24 09:44:36 -0300

    feat: adiciona compreensão de linguagem natural ao bot para o primeiro envio do menu

--- src/main.py
+++ src/main.py
@@ -4,40 +4,52 @@ from telebot.util import quick_markup
 from datetime import datetime
 from core.engine import *
 
-TOKEN = ""
+TOKEN = "8513074082:AAFLGcnPjAjGuTar9LjOTzMEXIGOzx56FOM"
 bot = telebot.TeleBot(TOKEN)
 
 user_data = {}
 idade = 0
+
 # /start
 @bot.message_handler(commands=['start'])
 def start(message):
     user_id = message.from_user.id
-    user_data[user_id] = {}
 
-    # Cria botões inline para escolha da faixa etária/grupo
+    user_data[user_id] = {"started_once": True}
+
+    menu(message.chat.id)
+
+# Cria botões inline para escolha da faixa etária/grupo
+def menu(chat_id):
     markup = quick_markup({
-        'Gestante': {'callback_data': 'grupo_gestante'},
-        'Criança': {'callback_data': 'grupo_crianca'},
-        'Adolescente e Jovem': {'callback_data': 'grupo_adolescente'},
-        'Adulto': {'callback_data': 'grupo_adulto'},
-        'Idoso': {'callback_data': 'grupo_idoso'}
+        'Gestante 🤰': {'callback_data': 'grupo_gestante'},
+        'Criança 👶': {'callback_data': 'grupo_crianca'},
+        'Jovens 🧑': {'callback_data': 'grupo_adolescente'},
+        'Adulto 🧑‍💼': {'callback_data': 'grupo_adulto'},
+        'Idoso 👴': {'callback_data': 'grupo_idoso'}
     }, row_width=3)
     
-    bot.send_message(message.chat.id, "Escolha o grupo para consultar as vacinas:", reply_markup=markup)
+    bot.send_message(chat_id, "Escolha o grupo para consultar as vacinas:", reply_markup=markup)
 
 @bot.message_handler(commands=['procurar'])
 def procurar(message):
-    if user_data:
-        if len(message.text.split()) < 2:
-            bot.reply_to(message, 'Use: /procurar nome da vacina')
-            return
-        vacina = ' '.join([x for x in message.text.split()[1:]]).rstrip().lower()
-        bot.reply_to(message, procura_vacina(vacina), parse_mode='HTML')
-    else:
-        bot.reply_to(message, 'Use: /start antes')
+    if len(message.text.split()) < 2:
+        bot.reply_to(message, 'Use: /procurar nome da vacina')
         return
+    vacina = ' '.join([x for x in message.text.split()[1:]]).rstrip().lower()
+    bot.reply_to(message, procura_vacina(vacina), parse_mode='HTML')
 
+# Inicia o bot a partir de qualquer mensagem
+
+@bot.message_handler(func=lambda message: message.text and not message.text.startswith('/'))
+def start_natural(message):
+    user_id = message.from_user.id
+
+    if user_id not in user_data:
+        user_data[user_id] = {"started_once": True}
+        start(message)
+    else:
+        bot.reply_to(message, 'Use: /start para exibir o menu novamente')
 
 # Callbacks - editam a primeira mensagem e retornar a vacina de acordo com a situação
 @bot.callback_query_handler(func=lambda call: True)
@@ -60,6 +72,10 @@ def callback_handler(call):
     
     if call.data.endswith('idoso'):
         edita_mensagem('Idoso')
+
+    # Exibe o menu novamente após a exibição das vacinas
+
+    bot.answer_callback_query(call.id)
 
 # Iniciar bot
 bot.polling()

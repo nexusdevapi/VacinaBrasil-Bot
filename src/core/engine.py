0c557e77b1179e93feb5cdc344a9cc483f34e929
Gabriel <gabriel.yfujimoto@gmail.com>
2026-06-01 01:07:21 -0300

    refactor: Muda formatação da resposta da IA

--- src/core/engine.py
+++ src/core/engine.py
@@ -7,12 +7,15 @@ from data_handler.loader import *
 def pega_vacina(periodo):
     resultado = ''
     data = carregar_json('calendario_vacinas.json')
-    data = [info for info in data if info['grupo'].split()[0] == periodo]
+    data = [info for info in data if info['grupo'].split()[0] == periodo.split('_')[0]]
+
+    if periodo.endswith('json'):
+        return data
 
     for info in data:
         if info['idade_texto'] != '':
-            resultado += f'🗓️ <b>{info["idade_texto"].replace(chr(10), " ")}</b>:\n\n' if resultado == '' else f'───────────────────\n\n🗓️ <b>{info["idade_texto"].replace(chr(10), " ")}</b>:\n\n'
-        resultado += f'💉 {info["vacina"].replace(chr(10), " ")}\n    • {info["dose"].replace(chr(10), " ")}\n\n'
+            resultado += f'🗓️ <b>{info['idade_texto'].replace('\n', ' ')}</b>:\n\n' if resultado == '' else f'───────────────────\n\n🗓️ <b>{info['idade_texto'].replace('\n', ' ')}</b>:\n\n'
+        resultado += f'💉 {info['vacina'].replace('\n', ' ')}\n    • {info['dose'].replace('\n', ' ')}\n\n'
     return resultado
 
 def procura_vacina(nome):
@@ -177,11 +180,117 @@ def formatar_cobertura(nome, soma, total):
 def resto(texto):
     return 'Olá, não entendi sua pergunta. Poderia fazê-lá novamente?'
 
+def dia(nascimento):
+    try:
+        dia, mes, ano = map(int, nascimento.split('/'))
+        nasc = date(ano, mes, dia)
+        atual = date.today()
+        ano = 0
+
+        if atual.year == nasc.year:
+            mes = atual.month - nasc.month
+            return idade(f'{mes} months')
+        else:
+            ano = atual.year - nasc.year
+
+        if not (nasc.month, nasc.day) < (atual.month, atual.day) and ano != 0:
+            ano -= 1
+
+        return idade(f'{ano} years')
+    except Exception as e:
+        return e
+   
+def idade(id):
+    ano = mes = semana = -1
+    resultado = ''
+    max = mini = 0
+    if 'semana' in id or 'week' in id:
+        if id == 'semana':
+            semana = 1
+        semana = int(id.split()[0])
+    elif 'month' in id:
+        mes = int(id.split()[0])
+    elif 'year' in id:
+        ano = int(id.split()[0])
+        if ano == 1:
+            ano = -1
+            mes = 12
+    
+    if semana != -1:
+        periodo = 'Gestante_json'
+    elif 0 <= ano <= 9 or 0 <= mes <= 15:
+        periodo = 'Criança_json'
+    elif 10 <= ano <= 24:
+        periodo = 'Adolescente_json'
+    elif 25 <= ano <= 29:
+        return pega_vacina('Adulto')
+    elif ano >= 60:
+        return pega_vacina('Idoso')
+
+    data = pega_vacina(periodo)
+
+    for info in data:
+        if info['idade_min'] is not None:
+            mini = info["idade_min"]
+
+        if info['idade_max'] is not None:
+            max = info["idade_max"]
+
+        if info['idade_min'] is None:
+            info['idade_min'] = mini
+
+        if info['idade_max'] is None:
+            info["idade_max"] =  max
+
+        if semana != -1 and ano == mes == -1:
+            match = re.search(r'(\d+)ª', info['dose'])
+            if match and semana < int(match.group(1)):
+                return resultado
+            if info['idade_texto'] != '':
+                resultado += f'🗓️ <b>{info['idade_texto'].replace('\n', ' ')}</b>:\n\n' if resultado == '' else f'───────────────────\n\n🗓️ <b>{info['idade_texto'].replace('\n', ' ')}</b>:\n\n'
+            resultado += f'💉 {info['vacina'].replace('\n', ' ')}\n    • {info['dose'].replace('\n', ' ')}\n\n'
+        elif mes == 0 and ano == 0:
+            if info['idade_min'] == info['idade_max'] == 0:
+                if info['idade_texto'] != '':
+                    resultado += f'🗓️ <b>{info['idade_texto'].replace('\n', ' ')}</b>:\n\n'
+                resultado += f'💉 {info['vacina'].replace('\n', ' ')}\n    • {info['dose'].replace('\n', ' ')}\n\n'
+        elif mes > 0 and ano == 0:
+            if info['idade_texto'].endswith('anos'):
+                return resultado
+
+            if info['idade_max'] is None:
+                if mes >= info['idade_min']:
+                    if info['idade_texto'] != '':
+                        resultado += f'🗓️ <b>{info['idade_texto'].replace('\n', ' ')}</b>:\n\n' if resultado == '' else f'───────────────────\n\n🗓️ <b>{info['idade_texto'].replace('\n', ' ')}</b>:\n\n'
+                    resultado += f'💉 {info['vacina'].replace('\n', ' ')}\n    • {info['dose'].replace('\n', ' ')}\n\n'
+            else:
+                if info['idade_min'] <= mes <= info['idade_max'] or mes >= info['idade_min']:
+                    if info['idade_texto'] != '':
+                        resultado += f'🗓️ <b>{info['idade_texto'].replace('\n', ' ')}</b>:\n\n' if resultado == '' else f'───────────────────\n\n🗓️ <b>{info['idade_texto'].replace('\n', ' ')}</b>:\n\n'
+                    resultado += f'💉 {info['vacina'].replace('\n', ' ')}\n    • {info['dose'].replace('\n', ' ')}\n\n'
+        else:
+            if info['idade_texto'].endswith('meses'):
+                continue
+
+            if info['idade_max'] is None:
+                if ano >= info['idade_min']:
+                    if info['idade_texto'] != '':
+                        resultado += f'🗓️ <b>{info['idade_texto'].replace('\n', ' ')}</b>:\n\n' if resultado == '' else f'───────────────────\n\n🗓️ <b>{info['idade_texto'].replace('\n', ' ')}</b>:\n\n'
+                    resultado += f'💉 {info['vacina'].replace('\n', ' ')}\n    • {info['dose'].replace('\n', ' ')}\n\n'
+            else:
+                if info['idade_min'] <= ano <= info['idade_max']:
+                    if info['idade_texto'] != '':
+                        resultado += f'🗓️ <b>{info['idade_texto'].replace('\n', ' ')}</b>:\n\n' if resultado == '' else f'───────────────────\n\n🗓️ <b>{info['idade_texto'].replace('\n', ' ')}</b>:\n\n'
+                    resultado += f'💉 {info['vacina'].replace('\n', ' ')}\n    • {info['dose'].replace('\n', ' ')}\n\n'
+    return resultado
+
 def resposta_ia(perg):
     functions = {
         "pega": pega_vacina,
         "procura": procura_vacina,
         "cobertura": consultar_cobertura,
+        "dia": dia,
+        "idade": idade,
         "resto": resto
     }
 
@@ -192,7 +301,12 @@ def resposta_ia(perg):
     - pega: searchs vaccine for age groups like child, adult or elderly. choose one of the arguments[Gestante, Criança, Adolescente e Jovem, Adulto, Idoso] accordingly to the age group
     - procura: uses the name of a vaccine for example dengue or hepatite.
     - cobertura: uses the main regions of brazil. arguments[Norte, Nordeste, Centro-Oeste, Sul, Sudeste]
+    - dia: transforms a date in this format: dd/mm/yyyy.
+    - idade: uses the age of the user and classifies them in one of the three data types(years, months or weeks). For the arguments return a string in this format: x years or y months or z weeks. Don't pick any value resembling a date for this function. If the age passes 99 return 99 years. Keep the same value as the user gave just put a type on them.
     - resto: uses anything unrelated to the other functions.
+
+    for the idade function:
+    Dont convert the values. Keep them the same. Change semana to week
 
     User request:
     "{perg}"
@@ -202,14 +316,16 @@ def resposta_ia(perg):
         "funcao": "function_name",
         "args": "arguments"
     }}
+
+    Return only valid JSON.
+    Dont use '''
     """
     response = ollama.chat(
-        model='mistral',
+        model='qwen2.5',
         messages=[
             {'role': 'user', 'content': prompt}
         ]
     )
-
     selected = response['message']['content'].strip()
     selected = json.loads(selected)
 
